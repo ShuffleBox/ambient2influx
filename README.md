f22fab8bb9f3b58f382dd46a7c1aef3e700e28ee
Garret Rumohr <garretr@gmail.com>
2025-10-14 08:00:25 -0500

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ambient2influx
 flask application to receive ambient weather ws-29002 payload and forward it to influxdb v1
+
+The plan is to receive the http payload from the Ambient Weather station and forward it to influxDB.
---
+++ ambient2influx/app/routes.py
@@ -0,0 +1,40 @@
+import os
+
+from datetime import datetime
+import pytz
+from influxdb import InfluxDBClient
+
+from app import app
+from flask import request
+
+
+#import ipdb
+from pprint import pprint
+
+@app.route('/ambient2influx/')
+def ambient2influx():
+    #convert args to python dictionary
+    payload_dict = request.args.to_dict()
+    #ipdb.set_trace()
+
+    #make influxDB client
+    influx_client = InfluxDBClient(os.getenv('INFLUX_SERVER'),
+                                os.getenv('INFLUX_PORT'),
+                                os.getenv('INFLUX_USER'),
+                                os.getenv('INFLUX_PASSWORD'),
+                                os.getenv('INFLUX_DATABASE'))
+
+    payload = {}
+    payload['measurement'] = 'Ambient Weather Station Readings'
+    payload['fields'] = payload_dict
+    commit = []
+    commit.append(payload)
+    #influx_client.write_points(commit)
+    pprint(commit)
+    return "Hello, ambient"
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
