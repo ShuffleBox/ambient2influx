354b180d8acece8e3a04acb07b4e9354f2a81e67
Garret Rumohr <garretr@gmail.com>
2025-10-15 12:21:55 -0500

    iterate to make measurements values instead of strings

--- ambient2influx/app/routes.py
+++ ambient2influx/app/routes.py
@@ -12,8 +12,30 @@ from pprint import pprint
 @app.route('/ambient2influx/')
 def ambient2influx():
     #convert args to python dictionary
-    payload_dict = request.args.to_dict()
+    raw_dict = request.args.to_dict()
     
+    #need to modify the payload to properly type the contents
+    payload_dict = {}
+    for key, value in raw_dict.items():
+        if key == "PASSKEY":
+            continue
+        elif key == "dateutc":
+            try:
+                # Parse device-provided time (example: 2025-10-15 17:09:32)
+                timestamp = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+                payload_dict[key] = timestamp.isoformat() + 'Z'
+            except Exception as e:
+                print(f"Could not parse dateutc: {e}")
+            continue  # skip adding to fields
+        try:
+            if '.' in value:
+                payload_dict[key] = float(value)
+            else:
+                payload_dict[key] = int(value)
+        except ValueError:
+            payload_dict[key] = value
+        
+            
     #make influxDB client
     influx_client = InfluxDBClient(os.getenv('INFLUX_SERVER'),
                                 os.getenv('INFLUX_PORT'),
